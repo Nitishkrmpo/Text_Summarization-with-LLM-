fff6472d62dddea6f04306db1c3b41ee6ce67b8f
Nitish <nitishkumarmpo@gmail.com>
2025-04-01 01:15:48 +0530

    This is My first Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup
+setup(
+    name='Text-Summarization with LLM',
+    version='0.0.1',
+    author='Group-8',
+    author_email=['22051774@kiit.ac.in', '22052029@kiit.ac.in', '22052031@kiit.ac.in', '22052035@kiit.ac.in', '22051689@kiit.ac.in', '22052027@kiit.ac.in'],
+    packages = find_packages(),
+    install_requires=[]
+)
+
+##Setup wale file se  Text-Summmaization with  LLM 
